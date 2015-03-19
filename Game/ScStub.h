a4fb1c54982213dc4bd03e34922e2b923232b53f
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2015-03-19 13:00:55 +0000

    Added method signatures for ScStub virtual member functions. Added note about possible values for enableStatus field, which is not a true boolean.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/API/ForcedExports@917 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

--- Game/ScStub.h
+++ Game/ScStub.h
@@ -7,15 +7,25 @@
 
 namespace OP2ForcedExport
 {
+	class ScStubCreator;
+
 	class ScStub
 	{
 	public:
-		virtual int F1();	// ** Dummy, force vtbl entry, try checking actual code
+		virtual ScStub* Release(int bDelSelf);		// 0x00
+		virtual ScStubCreator* GetScStubCreator();	// 0x04
+		virtual void SetA1();						// 0x08 (Set enableStatus = 2) **
+		virtual void Enable();						// 0x0C (Set enableStatus = 1)
+		virtual void Disable();						// 0x10 (Set enableStatus = 0)
+		virtual int Save(StreamIO* savedGameFile);	// 0x14 (saves enableStatus field)
+		virtual int Load(StreamIO* savedGameFile);	// 0x18 (loads enableStatus field)
+		virtual ~ScStub();							// 0x1C (Destructor)
+		virtual int GetOffsetA2();					// 0x20 (returns scStubIndex * 16 + enableStatus + 1) **
 
 	public:
 		// vtbl				// 0x0 
 		int scStubIndex;	// 0x4 
-		int enableStatus;	// 0x8 
+		int enableStatus;	// 0x8 (Values 0, 1, 2?)
 		int unknown1;		// 0xC  **
 		int unknown2;		// 0x10 **
 	};
