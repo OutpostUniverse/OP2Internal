150e9756ed3024a99c881312b43aedfbd918db0f
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2015-03-20 17:34:28 +0000

    Named previously unknown ScStub member functions Initialize and Checksum.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/API/ForcedExports@918 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

--- Game/ScStub.h
+++ Game/ScStub.h
@@ -14,13 +14,13 @@ namespace OP2ForcedExport
 	public:
 		virtual ScStub* Release(int bDelSelf);		// 0x00
 		virtual ScStubCreator* GetScStubCreator();	// 0x04
-		virtual void SetA1();						// 0x08 (Set enableStatus = 2) **
+		virtual void Initialize();					// 0x08 (Set enableStatus = 2) ** [Initialize?]
 		virtual void Enable();						// 0x0C (Set enableStatus = 1)
 		virtual void Disable();						// 0x10 (Set enableStatus = 0)
-		virtual int Save(StreamIO* savedGameFile);	// 0x14 (saves enableStatus field)
-		virtual int Load(StreamIO* savedGameFile);	// 0x18 (loads enableStatus field)
+		virtual int Save(StreamIO* savedGameFile);	// 0x14 (Saves enableStatus field)
+		virtual int Load(StreamIO* savedGameFile);	// 0x18 (Loads enableStatus field)
 		virtual ~ScStub();							// 0x1C (Destructor)
-		virtual int GetOffsetA2();					// 0x20 (returns scStubIndex * 16 + enableStatus + 1) **
+		virtual int Checksum();						// 0x20 (Returns scStubIndex * 16 + enableStatus + 1)
 
 	public:
 		// vtbl				// 0x0 
