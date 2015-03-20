efd5a3e95948854f10a5fee04ed8c5beb59f117c
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2015-03-20 18:35:57 +0000

    Added ScStub:Group class.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/API/ForcedExports@925 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

---
+++ Game/ScStub/ScStubGroup.h
@@ -0,0 +1,69 @@
+#ifndef ScStubGroup_H
+#define ScStubGroup_H
+
+#include "ScStub.h"
+
+
+enum UnitClassifactions;
+enum map_id;
+
+
+namespace OP2ForcedExport
+{
+
+	// Size: 0x14
+	struct UnitNode
+	{
+		UnitNode* prev;						// 0x00
+		UnitNode* next;						// 0x04
+		Unit* unit;							// 0x08
+		int issueCommandTick;				// 0x0C ** [Set to 0xFFF00000 when adding unit to group, related to gameTick and deleteWhenEmpty]
+		UnitClassifactions classification;	// 0x10
+		// ----
+	};
+
+	// Size: 0x14
+	class TargetCount
+	{
+	public:
+	public:
+		int a1;					// 0x00 **
+		void* a2;				// 0x04 **
+		int groupScStubIndex;	// 0x08
+		int a3;					// 0x0C **
+		int a4;					// 0x10 **
+		// ----
+	};
+
+
+	class Group : public ScStub
+	{
+	public:
+		// ----  [New virtual functions]
+		virtual int HasFired();					// 0x24
+		virtual void AddUnit(Unit* unit);		// 0x28
+		virtual void RemoveUnit(Unit* unit);	// 0x2C
+		virtual void A1();						// 0x30 **
+		virtual void A2();						// 0x34 **
+		// ----
+
+	public:
+		int a1;							// 0x14  **
+		int a2;							// 0x18  **
+		int lastFreeUnitNodeIndex;		// 0x1C  ** (+= 1)
+		int a4;							// 0x20  ** (+= 1)
+		UnitNode unitNode[32];			// 0x24 (Linked list node storage for pointers to up to 32 units)
+		UnitNode* unitByType[16];		// 0x2A4 (Indexed by UnitClassifactions)
+		TargetCount* targetCount;		// 0x2E4 (Object to keep track of target counts)
+		UnitNode* unitLinkedListHead;	// 0x2E8 (Ordered by UnitClassifactions)
+		UnitNode* unitLinkedListTail;	// 0x2EC (Ordered by UnitClassifactions)
+		int numUnits;					// 0x2F0 (Returned by TotalUnitCount())
+		int ownerPlayerNum;				// 0x2F4 (All units in group should belong to this player)
+		int deleteWhenEmptyTick;		// 0x2F8 (Initialized to MAX_INT, set to TethysGame.tick + 10000 by SetDeleteWhenEmpty(true))
+		int bSetLights;					// 0x2FC (Initialized to 1 (on))
+	};
+
+}	// End namespace
+
+
+#endif
