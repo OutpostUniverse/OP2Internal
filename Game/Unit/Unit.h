ef739b6661d81089b17267339614fa28d89e8afc
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2015-04-04 05:21:33 +0000

    Added struct for unit flags. Preliminary declaration. May need to adjust to deal with multiple uses for some bits.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/API/ForcedExports@935 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

--- Game/Unit/Unit.h
+++ Game/Unit/Unit.h
@@ -10,6 +10,40 @@ namespace OP2ForcedExport
 	class UnitTypeInfo;
 	class Viewport;
 
+	struct UnitFlags {
+		unsigned int lightsOn:1;					// 0x00 0x00000001
+		unsigned int isVehicle:1;					// 0x01 0x00000002
+		unsigned int isBuilding:1;					// 0x02 0x00000004
+		unsigned int _3:1;							// 0x03 0x00000008 **
+		unsigned int isOffensive:1;					// 0x04 0x00000010 (1 = show attack/patrol buttons, 0 = no buttons)
+		unsigned int doubleFireRate:1;				// 0x05 0x00000020 (Dual turret)
+		unsigned int _6:1;							// 0x06 0x00000040 (If 0 and !isVehicle, does "moDelSelf" on "ctMoDeath")
+		unsigned int damageIncrease:1;				// 0x07 0x00000080 (150% damage for guard posts connected by tubes to a Command Center)
+		unsigned int canExplode:1;					// 0x08 0x00000100 ** ?
+		unsigned int _9:1;							// 0x09 0x00000200 **
+		unsigned int _10:1;							// 0x0A 0x00000400 **
+		unsigned int _11:1;							// 0x0B 0x00000800 **
+		unsigned int isEden:1;						// 0x0C 0x00001000 (Used to display correct chasis for combat vehicles)
+		unsigned int enabledPower:1;				// 0x0D 0x00002000 ** (DisasterDetected)
+		unsigned int enabledWorkers:1;				// 0x0E 0x00004000 ** (DisasterNoFirstWarn)
+		unsigned int enabledScientists:1;			// 0x0F 0x00008000 ** (DisasterNoSecondWarn)
+		unsigned int _16:1;							// 0x10 0x00010000 ** (DoAftershock) [Earthquake]
+		unsigned int isDead:1;						// 0x11 0x00020000 (Used by IsLive)
+		unsigned int isStickyfoamed:1;				// 0x12 0x00040000 ** (Building: isInfected)
+		unsigned int isEMPed:1;						// 0x13 0x00080000
+		unsigned int isESGed:1;						// 0x14 0x00100000
+		unsigned int _21:1;							// 0x15 0x00200000 **
+		unsigned int preventExplosion:1;			// 0x16 0x00400000 (Prevents EMP blasts and harmful explosions upon building death)
+		unsigned int oppFiredUpon:1;				// 0x17 0x00800000 (Allows opponent units to auto target this building)
+		unsigned int isUndocking:1;					// 0x18 0x01000000 (Convec/CargoTruck? being moved up out of storage bay after pickup/dropoff)
+		unsigned int hasSpecialTarget:1;			// 0x19 0x02000000 (CreateSpecialTarget, ClearSpecialTarget)
+		unsigned int _26:1;							// 0x1A 0x04000000 **
+		unsigned int forceFullLightVisibility:1;	// 0x1B 0x08000000 **
+		unsigned int isInvisible:1;					// 0x1C 0x10000000 (for disasters (and buildings)) (before they appear they exist)
+		unsigned int noAutoTarget:1;				// 0x1D 0x20000000 ** (HasSpecialTarget?)
+		unsigned int _30:1;							// 0x1E 0x40000000 **
+		unsigned int _31:1;							// 0x1F 0x80000000 **
+	};
 
 	// Size: 0x54
 	class Unit
@@ -74,10 +108,10 @@ namespace OP2ForcedExport
 		char b1;						// 0x32 **
 		char b2;						// 0x33 **
 		int* b3;						// 0x34 ** WaypointStruct?
-		int b4;							// 0x38 **
+		int b4;							// 0x38 ** nextWaypoint?
 		int actionTimer;				// 0x3C
 		int animationAndFrameIndex;		// 0x40
-		int flags;						// 0x44 
+		UnitFlags flags;				// 0x44 
 		int b5[3];						// 0x48 **
 		// ----
 	};
