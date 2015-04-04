bceda448deaf730c7716c46caa7be1b6c4d0536f
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2015-04-04 05:38:01 +0000

    Added preliminary definition for Path and PathFinder classes.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/API/ForcedExports@936 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

---
+++ Game/Unit/Path.h
@@ -0,0 +1,51 @@
+
+
+#ifndef Path_H
+#define Path_H
+
+
+namespace OP2ForcedExport
+{
+
+	struct Path
+	{
+		union {
+			Path* nextFree;			// 0x00
+			int numWaypoints;		// 0x00 ** See offset 0x74
+		};
+		int lastWaypointIndex;		// 0x04
+		//
+		int waypointList[8];		// 0x14 ** Packed bitfield
+		//
+		int flags;					// 0x34 **
+		Path* next;					// 0x38 **
+		int a0Enum;					// 0x3C ** (0 = Move/Dock/DockEG/StandGround/CargoRoute/Patrol/Build, 3 = RemoveWall)
+		//
+		int currentPixelX;			// 0x48 ** (or start)
+		int currentPixelY;			// 0x4C ** (or start)
+		int destTileX;				// 0x50
+		int destTileY;				// 0x54
+		int a1;						// 0x58 ** Copy of 0x5C
+		int a2;						// 0x5C ** (0, sign extend of 0x60)
+		char rotationSpeed;			// 0x60
+		char a3;					// 0x61 ** (padding?)
+		char a4;					// 0x62 ** (padding?)
+		char a5;					// 0x63 ** (padding?)
+		int a6;						// 0x64 ** (20, copy of 0x70)
+		int a7;						// **
+		int a8;						// **
+		int a9;						// 0x70 ** (Used as range in call to Rand, set using results)
+		int numWaypoints;			// 0x74
+		int currentWaypoint;		// 0x78
+		char direction[0];			// 0x7C ** (0..7: 0 = Right, increases clockwise, 45 degree increments)
+		int a10;					// 0xBC **
+		int a11;					// 0xC0 **
+		//
+		int a12Enum;				// 0xCC ** (0, 1, 2, 3)
+		int distance;				// 0xD0
+	};
+
+}	// End namespace
+
+
+#endif
---
+++ Game/Unit/PathFinder.h
@@ -0,0 +1,33 @@
+
+
+#ifndef Path_H
+#define Path_H
+
+
+namespace OP2ForcedExport
+{
+
+	class Vehicle;
+
+
+	// BaseAddress: 0x551340
+	class PathFinder
+	{
+	public:
+		bool __fastcall (a0*)(int tileX, int tileY);			// 0x00 (Function pointer)
+		int __fastcall (a1*)(int startTileX, int startTileY);	// 0x04 (Function pointer)
+		Vehicle* pathFindingUnit;	// 0x08
+		int startTileX;		// 0x0C
+		int startTileY;		// 0x10
+		int destTileX;		// 0x14
+		int destTileY;		// 0x18
+		int distance;		// 0x1C (rangeTileX^2 + rangeTileY^2) (squared distance)
+		int rangeTileX;		// 0x20
+		int rangeTileY;		// 0x24
+		// ...
+	};
+
+}	// End namespace
+
+
+#endif
